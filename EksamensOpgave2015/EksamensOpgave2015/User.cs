28db23b55bab8c2a3b02c33ed69d5363f6396f33
Andreas <ajosefskov@gmail.com>
2015-05-05 14:38:30 +0200

    Username færdig!

--- EksamensOpgave2015/EksamensOpgave2015/User.cs
+++ EksamensOpgave2015/EksamensOpgave2015/User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.Text.RegularExpressions;
 
 namespace EksamensOpgave2015
 {
@@ -34,10 +35,35 @@ namespace EksamensOpgave2015
             get;
             set
             {
-                if (userName.All(c => c < 0 || 9 < c || c < 'a' || 'z' < c))
+                if (userName.All(c => !(c >= 0 || 9 >= c || c >= 'a' || 'z' >= c || c == '_')))
                 {
-                        throw new ArgumentOutOfRangeException();
+                    throw new ArgumentOutOfRangeException();
                 }
+            }
+        }
+
+        private string email
+        {
+            get;
+            set
+            {
+                string[] split = email.Split('@');
+                if (split[0].All(c => !(c >= 0 || 9 >= c || c >= 'a' || 'z' >= c || c >= 'A' || 'Z' >= c
+                    || c == '.' || c == '-' || c == '_')))
+                    throw new ArgumentOutOfRangeException();
+
+                else if (split[1].All(c => !(c >= 0 || 9 >= c 
+                    || c >= 'a' || 'z' >= c || c >= 'A' || 'Z' >= c || c == '-' || c == '.')))
+                    throw new ArgumentOutOfRangeException();
+
+                char checkFirstLetter = split[1].First();
+                if (checkFirstLetter == '.' || checkFirstLetter == '-')
+                    throw new ArgumentOutOfRangeException();
+
+                char checkLastLetter = split[1].First();
+                if (checkLastLetter == '.' || checkLastLetter == '-')
+                    throw new ArgumentOutOfRangeException();
+
             }
         }
         public User(int ID, string firstName, string lastName,
