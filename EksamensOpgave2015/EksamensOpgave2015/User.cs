6c30014f2186ad2b65aa5a68085cdfba85d70eef
Andreas <ajosefskov@gmail.com>
2015-05-05 13:15:12 +0200

    Leger med User

--- EksamensOpgave2015/EksamensOpgave2015/User.cs
+++ EksamensOpgave2015/EksamensOpgave2015/User.cs
@@ -8,5 +8,30 @@ namespace EksamensOpgave2015
 {
     class User
     {
+        private int ID { get; set; }
+        private string firstName
+        {
+            get;
+            set
+            {
+                if (firstName == null)
+                    throw new ArgumentException();
+            }
+        }
+
+        private string lastName
+        {
+            get;
+            set
+            {
+                if (lastName == null)
+                    throw new ArgumentException();
+            }
+        }
+        public User(int ID, string firstName, string lastName,
+            string username, string email, string balance)
+        {
+
+        }
     }
 }
