3428a6e713d6ccdce63a58dbb7374ae764dc188a
Andreas <ajosefskov@gmail.com>
2015-05-06 14:08:34 +0200

    Forsøger at læse filen ind

--- EksamensOpgave2015/EksamensOpgave2015/LineSystem.cs
+++ EksamensOpgave2015/EksamensOpgave2015/LineSystem.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace EksamensOpgave2015
 {
     public class LineSystem
     {
+        public User user { get; set; }
+        public Product product { get; set; }
+        public Transaction transaction { get; set; }
+
+        public List<User> UserList = new List<User>();
+        public List<Product> ProductList = new List<Product>();
+        public List<Transaction> TransactionList = new List<Transaction>();
+
         public LineSystem(User user, Product product, Transaction transaction)
         {
 
@@ -15,17 +24,51 @@ namespace EksamensOpgave2015
 
         public void BuyProduct(User user, Product product)
         {
-
+            user.balance = user.balance - product.price;
         }
 
-        public void AddCreditsToAccount(User user, Transaction transaction.fdsfsdg)
+        public void AddCreditsToAccount(User user, Transaction transaction)
         {
+            string text = transaction.ToString();
 
+            user.balance = user.balance + transaction.amount;
+            System.IO.File.WriteAllText("log.txt", text);
         }
 
         public void ExecuteTransaction(Transaction transaction) 
         {
 
+        }
+
+        public void GetProduct()
+        {
+
+        }
+
+        public void GetUser()
+        {
+
+        }
+
+        public void GetTransactionList()
+        {
+
+        }
+
+        public void GetActiveProducts()
+        {
+
+        }
+
+        public void ReadFile()
+        {
+            var reader = new StreamReader(File.OpenRead(@"products.csv"));
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+            }
         }
 
 
