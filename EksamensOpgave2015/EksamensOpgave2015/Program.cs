9940c09152c06ac40ec45a371f1406a9f52caa2e
Andreas Josef Skov <ajs14@student.aau.dk>
2015-05-08 12:39:31 +0200

    no message

--- EksamensOpgave2015/EksamensOpgave2015/Program.cs
+++ EksamensOpgave2015/EksamensOpgave2015/Program.cs
@@ -13,11 +13,11 @@ namespace EksamensOpgave2015
             Linesystem linesystem = new Linesystem();
             LinesystemCLI cli = new LinesystemCLI(linesystem);
             cli.Start();
-            linesystem.AddUser(1, "Andreas", "Skov", "Store-D", "ajosefskov@gmail.com", 20);
-            linesystem.AddUser(2, "Magnus", "Møller", "MøllerOP", "magnus@gmail.com", 400);
-            linesystem.AddUser(3, "Marius", "LARGE", "StorBøs", "marius@gmail.com", 60);
-            linesystem.AddUser(4, "Jones", "TheHound", "Batman", "jonas@gmail.com", 5400);
-            linesystem.AddUser(5, "Charlie", "Charles", "Mojn", "charlie@gmail.com", 10);
+            //linesystem.AddUser(0, "Andreas", "Skov", "Store-D", "ajosefskov@gmail.com", 20);
+            //linesystem.AddUser(1, "Magnus", "Møller", "MøllerOP", "magnus@gmail.com", 400);
+            //linesystem.AddUser(2, "Marius", "LARGE", "StorBøs", "marius@gmail.com", 60);
+            //linesystem.AddUser(3, "Jones", "TheHound", "Batman", "jonas@gmail.com", 5400);
+            //linesystem.AddUser(4, "Charlie", "Charles", "Mojn", "charlie@gmail.com", 10);
             //linesystem.AddCreditsToAccount(linesystem.GetUser("stored"), 5000);
             //linesystem.AddCreditsToAccount(linesystem.GetUser("Batman"), 80);
             Console.ReadKey();
